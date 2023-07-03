297d2bdc64152d843f6b94d0c6fac4aca0faf5d7
TalisonMoura <talisonmoura13@gmail.com>
2023-07-02 21:42:14 -0300

    Criação das classes de Movie e testando alguns parametros.

---
+++ FilmesAPI/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Tittle);
+        Console.WriteLine(movie.Duration);
+    }
+
+}
---
+++ FilmesAPI/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmesAPI.Models;
+
+public class Movie
+{
+    public string Tittle { get; set; }
+    public string Gender { get; set; }
+    public int Duration { get; set; }
+}
