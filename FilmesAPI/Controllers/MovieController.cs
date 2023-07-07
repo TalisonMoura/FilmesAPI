e85279b30876bc48156776e91a18cc76a82f3215
TalisonMoura <talisonmoura13@gmail.com>
2023-07-06 21:15:21 -0300

    Criando a injeção de dependência dentro da MovieController

--- FilmesAPI/Controllers/MovieController.cs
+++ FilmesAPI/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers
@@ -7,28 +8,33 @@ namespace FilmesAPI.Controllers
     [Route("[Controller]")]
     public class MovieController : ControllerBase
     {
-        private static List<Movie> movies = new List<Movie>();
-        private static int id = 0;
+
+        private MovieContext _movieContext;
+
+        public MovieController(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
 
         [HttpPost]
         public IActionResult AddMovie([FromBody] Movie movie)
         {
-            movie.Id = id++;
-            movies.Add(movie);
+            _movieContext.Movies.Add(movie);
+            _movieContext.SaveChanges();
             return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
         }
 
         [HttpGet]
         public IEnumerable<Movie> GetMovies([FromQuery] int skip, [FromQuery] int take)
         {
-            return movies.Skip(skip).Take(take);
+            return _movieContext.Movies.Skip(skip).Take(take);
         }
 
 
         [HttpGet("{id}")]
         public IActionResult GetMovieById(int id)
         {
-            var movie = movies.FirstOrDefault(x => x.Id == id);
+            var movie = _movieContext.Movies.FirstOrDefault(x => x.Id == id);
             if (movie == null) return NotFound();
             return Ok(movie);
         }
